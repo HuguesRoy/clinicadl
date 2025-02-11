b1f0fc0ba471349285e26b04891f2bc405400a65
HuguesRoy <hugues.roy@gadz.org>
2025-02-11 11:52:55 +0100

    skull stripping

--- clinicadl/skull_stripping/skull_stripping.py
+++ clinicadl/skull_stripping/skull_stripping.py
@@ -7,6 +7,8 @@ from pathlib import Path
 
 import pandas as pd
 import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
 from torch.cuda.amp import autocast
 from torch.utils.data import DataLoader
 
@@ -23,6 +25,73 @@ from .models import StripModel
 logger = getLogger("clinicadl.quality-check")
 
 
+def compute_padding(input_size: int, target_size: int) -> tuple:
+    pad = (target_size - input_size) // 2
+    if (target_size - input_size) % 2 == 1:
+        return (pad, pad + 1)
+    else:
+        return (pad, pad)
+
+
+class OrientationSynthStrip:
+    def __call__(self, input_image: torch.Tensor):
+        # transform RAS to LIA orientation (entry: RAS, shape (1,x,y,z))
+        return input_image.permute((0, 1, 3, 2)).flip(2).flip(1)
+
+
+class PaddingSynthStrip:
+    def __call__(self, input_image: torch.Tensor):
+        # clip to SynthStrip compatible image resolution (x64 factor)
+        target_shape = tuple(
+            torch.clip(
+                torch.ceil(torch.tensor(input_image.shape[1:]) / 64).to(int) * 64,
+                192,
+                320,
+            )
+        )  # clip to min 192, 320 max size
+
+        # paddind
+        pad_1 = compute_padding(input_image.shape[-1], target_shape[-1])
+        pad_2 = compute_padding(input_image.shape[-2], target_shape[-2])
+        pad_3 = compute_padding(input_image.shape[-3], target_shape[-3])
+
+        return F.pad(input_image, (*pad_1, *pad_2, *pad_3), mode="constant", value=0)
+
+
+class NormalizeSynthStrip:
+    def __call__(self, input_image: torch.Tensor):
+        input_image -= input_image.min()
+        quantile = torch.quantile(
+            input_image, q=torch.tensor(0.99).to(input_image.dtype)
+        )
+        return (input_image / quantile).clip(0, 1)
+
+
+class OrientationBack:
+    def __call__(self, input_image: torch.Tensor):
+        # transform LIA to RAS orientation (entry: LIA, shape (1,x,y,z))
+        return input_image.flip(2).flip(1).permute((0, 1, 3, 2))
+
+
+class Unpadd:
+    def __call__(self, input_image: torch.Tensor):
+        # clip to SynthStrip compatible image resolution (x64 factor)
+        target_shape = tuple(
+            torch.clip(
+                torch.ceil(torch.tensor(input_image.shape[1:]) / 64).to(int) * 64,
+                192,
+                320,
+            )
+        )  # clip to min 192, 320 max size
+
+        # paddind
+        pad_1 = compute_padding(input_image.shape[-1], target_shape[-1])
+        pad_2 = compute_padding(input_image.shape[-2], target_shape[-2])
+        pad_3 = compute_padding(input_image.shape[-3], target_shape[-3])
+
+        return F.pad(input_image, (*pad_1, *pad_2, *pad_3), mode="constant", value=0)
+
+
 def skull_stripping_syntrip(
     caps_dir: Path,
     preprocessing_dict: Path,
@@ -36,7 +105,7 @@ def skull_stripping_syntrip(
     use_uncropped_image: bool = True,
 ):
     """
-    Performs MR image skull stripping on caps dataset
+    Performs MR image skull stripping on caps dataset using SynthStrip model
 
     Parameters
     -----------
@@ -89,7 +158,7 @@ def skull_stripping_syntrip(
             print("Unable to download required model for stripping process:", err)
 
     # Load stripping model
-    logger.debug("Loading SynStrip model.")
+    logger.debug("Loading SynthStrip model.")
     model.load_state_dict(torch.load(model_file))
     model.eval()
     if gpu:
@@ -111,16 +180,47 @@ def skull_stripping_syntrip(
         else:
             df = load_and_check_tsv(tsv_path, caps_dict, None)
 
-        dataset = CapsDatasetImage(caps_dir, df, preprocessing_dict)
-        dataloader = DataLoader(
-            dataset, num_workers=n_proc, batch_size=batch_size, pin_memory=True
+        transforms_synthstrip = transforms.Compose(
+            [
+                OrientationSynthStrip(),
+                PaddingSynthStrip(),
+                NormalizeSynthStrip(),
+            ]
         )
 
-        logger.info(f"Quality check will be performed over {len(dataloader)} images.")
+        dataset_synthstrip = CapsDatasetImage(
+            caps_dir, df, preprocessing_dict, transformation=transforms_synthstrip
+        )
 
-        for data in dataloader:
-            logger.debug(f"Processing subject {data['participant_id']}.")
-            inputs = data["image"]
+        dataset_unorm = CapsDatasetImage(
+            caps_dir,
+            df,
+            preprocessing_dict,
+        )
+
+        dataloader_synthstrip = DataLoader(
+            dataset_synthstrip,
+            num_workers=n_proc,
+            batch_size=batch_size,
+            pin_memory=True,
+        )
+
+        dataloader_unstrip = DataLoader(
+            dataset_unorm,
+            num_workers=n_proc,
+            batch_size=batch_size,
+            pin_memory=True,
+        )
+
+        logger.info(
+            f"Skull stripping will be performed over {len(dataloader_synthstrip.dataset)} images."
+        )
+
+        for data_synth, data_clear in zip(dataloader_synthstrip, dataloader_unstrip):
+            logger.debug(f"Processing subject {data_synth['participant_id']}.")
+            inputs = data_synth["image"]
+
+            clear_image = ""
             if gpu:
                 inputs = inputs.cuda()
             with autocast(enabled=amp):
@@ -133,7 +233,8 @@ def skull_stripping_syntrip(
             for idx, sub in enumerate(data["participant_id"]):
                 image_path_i = data["image_path"].parent()
                 name = data["image_path"].parts[-1].replace(".pt", "_skull_stripped.pt")
-                torch.save(outputs, image_path_i / name)
+                print(outputs[idx].shape)
+                torch.save(outputs[idx], image_path_i / name)
 
                 logger.debug(f" sub {sub} - session : {data["session_id"]} done")
 
